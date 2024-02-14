4549db97dcf3695dfcb77e6f4dde6ae9da2871fe
Jaimin-parikh <jaiminparikh12@gmail.com>
2024-02-14 16:15:39 +0530

    password encryption added

--- Controller/register.php
+++ Controller/register.php
@@ -7,6 +7,7 @@ $data = new Registration();
 $username = $_POST['username'];
 $email = $_POST['email'];
 $password = $_POST['password'];
+$password = password_hash($password, PASSWORD_BCRYPT);
 
 $data->register($username, $email, $password);
 
