e3c11e524a6c8d4a202b2d2774ae02bba873e5ef
Jaimin-parikh <jaiminparikh12@gmail.com>
2024-02-14 16:58:11 +0530

    Now we can insert only unique username .

--- Controller/login.php
+++ Controller/login.php
@@ -1,5 +1,23 @@
 <?php
 
+require('../Model/connection.php');
 
+$username = $_POST['username'];
+$password = $_POST['password'];
+// $passerr = null;
+
+$conn = connection\build_connection();
+
+$query = "SELECT * FROM registration WHERE username = '$username' ;";
+
+$result = $conn->query($query);
+$num = mysqli_num_rows($result);
+if($num==0){
+    header('Location: ../View/signup.html');
+    exit();
+}
+else{
+    
+}
 
 ?>
--- Controller/register.php
+++ Controller/register.php
@@ -4,10 +4,18 @@ require ('../Model/registration.php');
 use registration\Registration as Registration;
 
 $data = new Registration();
+
 $username = $_POST['username'];
 $email = $_POST['email'];
 $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
 
-$data->register($username, $email, $password);
+$query = "SELECT * FROM registration WHERE username = '$username'";
+$num = mysqli_num_rows($conn->query($query));
 
+if($num!=0){
+    echo "<h1 style='text-align:center'> User alredy exist</h1>";
+}
+else{
+    $data->register($username, $email, $password);
+}
 ?>
