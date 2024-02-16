30f8771d51db6179818bb34af0efe855eb9d1919
Jaimin-parikh <126648196+Jaimin-parikh@users.noreply.github.com>
2024-02-16 15:33:38 -0800

    pending password verification

--- Model/login.php
+++ Model/login.php
@@ -6,12 +6,15 @@ require_once('../Model/registration.php');
 
 use registration\Registration as Registration;
 
+// use function connection\build_connection;
+
 class Login
 {
-    public function login($username, $password)
+    public function login($username, $input)
     {
-        if (Registration::check_user($username)) {
-            echo "<h1 style = \"text-align:center\">Welcome!</h1>";
+        if (Registration::check_user($username)) { 
+            echo"<h1 style = \"text-align : center\">Welcome!</h1>";
+
         } else {
             header('Location: ../View/signup.html');
         }
