0550fc8919d8ac0ec718ca4d3a0143b509f64c99
Jaimin-parikh <126648196+Jaimin-parikh@users.noreply.github.com>
2024-02-16 11:23:26 -0800

    refactored

--- Controller/register.php
+++ Controller/register.php
@@ -9,13 +9,7 @@ $username = $_POST['username'];
 $email = $_POST['email'];
 $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
 
-$query = "SELECT * FROM registration WHERE username = '$username'";
-$num = mysqli_num_rows($conn->query($query));
+$register = new Registration();
+$register->register_user($username, $email, $password);
 
-if($num!=0){
-    echo "<h1 style='text-align:center'> User alredy exist</h1>";
-}
-else{
-    $data->register($username, $email, $password);
-}
 ?>
--- Model/registration.php
+++ Model/registration.php
@@ -1,25 +1,41 @@
 <?php
 
 namespace registration;
-require ('connection.php');
+
+use mysqli;
+
+require('connection.php');
 
 $conn = \connection\build_connection();
 
-class Registration{
+class Registration
+{
     public $username;
     public $email;
     public $password;
 
-    public function register($username, $email, $password){
+    public function check_user($username)
+    {
         global $conn;
-        $query = "INSERT INTO registration(`username`, `email`, `password`) VALUES ('$username','  $email',' $password');";
-        $conn->query($query);
-        if($conn){
-            header('Location: ../View/index.html');
-            exit();
+        $query = "SELECT * FROM registration WHERE username = '$username'";
+        $result = $conn->query($query);
+        if (mysqli_num_rows($result) > 0) {
+            return true;
+        } else
+            return false;
+    }
+    public function register_user($username, $email, $password)
+    {
+        global $conn;
+        if (Registration::check_user($username)) {
+            echo "<h1 style=\"text-align : center\">User Already Exists!</h1>";
+        } else {
+            $query = "INSERT INTO registration(`username`, `email`, `password`) VALUES ('$username','  $email',' $password');";
+            $conn->query($query);
+            if ($conn) {
+                header('Location: ../View/index.html');
+                exit();
+            }
         }
     }
 }
-
-
-?>
